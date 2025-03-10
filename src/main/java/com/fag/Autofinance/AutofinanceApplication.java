7ef8e0faef28b8834fc596fe5b253657252d6a53
Isabelly Castro <isa.castroo0303@gmail.com>
2025-03-10 16:54:59 -0300

    first commit

---
+++ src/main/java/com/fag/Autofinance/AutofinanceApplication.java
@@ -0,0 +1,13 @@
+package com.fag.Autofinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutofinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutofinanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fag/Autofinance/AutofinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fag.Autofinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutofinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
