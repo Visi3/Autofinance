ea5352cbe4b5bfe05f0e64c003b0d2db915492b9
Isabelly Castro <isa.castroo0303@gmail.com>
2025-04-22 10:25:20 -0300

    teste

--- src/main/java/com/fag/Autofinance/Services/VeiculoService.java
+++ src/main/java/com/fag/Autofinance/Services/VeiculoService.java
@@ -1,8 +1,6 @@
 package com.fag.Autofinance.services;
 
 import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.fag.Autofinance.entities.Veiculo;
