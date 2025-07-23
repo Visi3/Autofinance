776924540352601da0251471197ff840505e35fb
Isabelly Castro <isa.castroo0303@gmail.com>
2025-07-22 23:06:31 -0300

    melhorando tudo

--- src/main/java/com/fag/Autofinance/Controllers/ClienteController.java
+++ src/main/java/com/fag/Autofinance/Controllers/ClienteController.java
@@ -21,32 +21,44 @@ public class ClienteController {
         this.clienteService = clienteService;
     }
 
-    @PostMapping
-    public ResponseEntity<Cliente> criarOuAtualizarCliente(@RequestBody Cliente cliente) {
-        Cliente clienteSalvo = clienteService.salvarCliente(cliente);
-        return new ResponseEntity<>(clienteSalvo, HttpStatus.CREATED);
-    }
-
-    @GetMapping("/{cpfCnpj}")
-    public ResponseEntity<Cliente> buscarClientePorCpfCnpj(@PathVariable String cpfCnpj) {
-        Cliente cliente = clienteService.buscarClientePorCpfCnpj(cpfCnpj);
-        return ResponseEntity.ok(cliente);
-    }
-
-    @GetMapping
-    public List<Cliente> listarClientes() {
-        return clienteService.listarClientes();
-    }
-
-    @PutMapping("/{cpfCnpj}")
-    public ResponseEntity<Cliente> atualizarCliente(@PathVariable String cpfCnpj, @RequestBody @Valid Cliente cliente) {
-        Cliente atualizado = clienteService.atualizarCliente(cpfCnpj, cliente);
-        return ResponseEntity.ok(atualizado);
-    }
-
-    @DeleteMapping("/{cpfCnpj}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deletarCliente(@PathVariable String cpfCnpj) {
-        clienteService.deletarCliente(cpfCnpj);
-    }
+    /*
+     * @GetMapping
+     * public List<Cliente> listaTodos() {
+     * return clienteService.listarTodos();
+     * }
+     * 
+     * 
+     * @PostMapping
+     * public ResponseEntity<Cliente> criarOuAtualizarCliente(@RequestBody Cliente
+     * cliente) {
+     * Cliente clienteSalvo = clienteService.salvarCliente(cliente);
+     * return new ResponseEntity<>(clienteSalvo, HttpStatus.CREATED);
+     * }
+     * 
+     * @GetMapping("/{cpfCnpj}")
+     * public ResponseEntity<Cliente> buscarClientePorCpfCnpj(@PathVariable String
+     * cpfCnpj) {
+     * Cliente cliente = clienteService.buscarClientePorCpfCnpj(cpfCnpj);
+     * return ResponseEntity.ok(cliente);
+     * }
+     * 
+     * @GetMapping
+     * public List<Cliente> listarClientes() {
+     * return clienteService.listarClientes();
+     * }
+     * 
+     * @PutMapping("/{cpfCnpj}")
+     * public ResponseEntity<Cliente> atualizarCliente(@PathVariable String
+     * cpfCnpj, @RequestBody @Valid Cliente cliente) {
+     * Cliente atualizado = clienteService.atualizarCliente(cpfCnpj, cliente);
+     * return ResponseEntity.ok(atualizado);
+     * }
+     * 
+     * @DeleteMapping("/{cpfCnpj}")
+     * 
+     * @ResponseStatus(HttpStatus.NO_CONTENT)
+     * public void deletarCliente(@PathVariable String cpfCnpj) {
+     * clienteService.deletarCliente(cpfCnpj);
+     * }
+     */
 }
